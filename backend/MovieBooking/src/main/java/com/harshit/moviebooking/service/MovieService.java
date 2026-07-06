f976adff00a403a717c439c4353f0531189bb2ce
harshit <harsshitsri@proton.me>
2026-07-06 21:56:52 +0530

    Add DTO layer, mapper layer and service architecture

--- backend/MovieBooking/src/main/java/com/harshit/moviebooking/service/MovieService.java
+++ backend/MovieBooking/src/main/java/com/harshit/moviebooking/service/MovieService.java
@@ -1,4 +1,19 @@
 package com.harshit.moviebooking.service;
 
+import com.harshit.moviebooking.dto.movie.MovieRequestDto;
+import com.harshit.moviebooking.dto.movie.MovieResponseDto;
+import com.harshit.moviebooking.entity.Movie;
+import com.harshit.moviebooking.mapper.MovieMapper;
+import com.harshit.moviebooking.repository.MovieRepo;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public interface MovieService {
+
+    MovieResponseDto createMovie(MovieRequestDto requestDto);
+
+    }
+
 }
+
