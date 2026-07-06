8fa97a47983318ba8b909f7bdc010343f4e180eb
harshit <harsshitsri@proton.me>
2026-07-06 19:06:49 +0530

    Initial Spring Boot project setup

---
+++ backend/MovieBooking/src/test/java/com/harshit/moviebooking/MovieBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshit.moviebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
