8f7342e6b6cd735531fc0ec28b330a960b4dcdd9
harshit <harsshitsri@proton.me>
2026-07-07 13:39:48 +0530

    Implement movie creation REST endpoint

--- backend/MovieBooking/src/main/java/com/harshit/moviebooking/service/impl/MovieServiceImpl.java
+++ backend/MovieBooking/src/main/java/com/harshit/moviebooking/service/impl/MovieServiceImpl.java
@@ -1,8 +1,15 @@
 package com.harshit.moviebooking.service.impl;
 
+import com.harshit.moviebooking.dto.movie.MovieRequestDto;
+import com.harshit.moviebooking.dto.movie.MovieResponseDto;
+import com.harshit.moviebooking.entity.Movie;
+import com.harshit.moviebooking.mapper.MovieMapper;
 import com.harshit.moviebooking.repository.MovieRepo;
 import com.harshit.moviebooking.service.MovieService;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Service
 public class MovieServiceImpl implements MovieService {
@@ -11,5 +18,21 @@ public class MovieServiceImpl implements MovieService {
 
     public MovieServiceImpl(MovieRepo movieRepository) {
         this.movieRepository = movieRepository;
+    }
+
+    @Override
+    public MovieResponseDto createMovie(MovieRequestDto requestDto) {
+
+        Movie movie = MovieMapper.toEntity(requestDto);
+
+        movie.setCreatedAt(LocalDateTime.now());
+        movie.setUpdatedAt(LocalDateTime.now());
+
+        movie.setAverageRating(BigDecimal.ZERO);
+        movie.setRatingCount(0);
+
+        Movie savedMovie = movieRepository.save(movie);
+
+        return MovieMapper.toResponseDto(savedMovie);
     }
 }
